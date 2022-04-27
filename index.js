0494c751ade6fc63d9899735782d644f167a4205
Giovani SFPV <giovanisfpv@gmail.com>
2022-04-27 00:19:06 -0300

    The end?

--- index.js
+++ index.js
@@ -5,18 +5,43 @@ const app = express();
 const PORT= 8080;
 
 
-//curl is a commando for test rest apis
+app.get("/camisa",(req, res)=>{
+    res.status(200)//a status senn to the client status betwenn 200-299
+    //means ok
+        .send({//send a data in this case a js object by default
+            //the client will recive an json file
+            color:"red",
+            size:"G"
+        });
+});
 
-//curl "ulr of the website"c 1
+//make the express POSTs with json filea
+app.use(express.json());
 
-//will recive the information of a website a.k.a
-//GET Request
+//the :id is a parameter
+app.post("camisa/:id",(req, res)=>{
 
-//curl -i "ulr of the website"
-//will return with the header usefull went work with
-//apis
+    //save the value pass in the url
+    const {id}= req.params;
 
-//curl
+    //went access a post route the user will also send info
+    //this info is save in somte that we call body (the body of the message)
+
+    //the commad used:
+    //
+    const {logo} = req.body;
+
+    if(!logo){
+        res.status(418)//>400 means error
+            .send({message:"we need a logo"});
+    }
+
+    res.send({
+        camisa:`camisa de id =${id} com o logo ${logo} ` 
+    });
+});
+
+
 
 app.listen(PORT,
            ()=> console.log("conectado"));
